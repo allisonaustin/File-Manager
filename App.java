da1ac198b2d9733820f0f91c7240f8f53ed7b848
Allison Austin <64452109+allisonaustin@users.noreply.github.com>
2021-04-08 15:38:42 -0700

    Added Main driver code and GUI class (App.java)

---
+++ App.java
@@ -0,0 +1,5 @@
+package CECS_277_Final_Proj;
+
+public class App{
+
+}
---
+++ FileManager.java
@@ -0,0 +1,9 @@
+package CECS_277_Final_Proj;
+
+public class FileManager{
+    public static void main(String args[]){
+        App app = new App();
+        app.go();
+    }
+
+}
